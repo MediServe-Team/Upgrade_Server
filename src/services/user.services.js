e1816c9bde5eb30c06b6c11872b545e6cc548ff9
PhucHoangVan <20521760@gm.uit.edu.vn>
2023-11-13 22:36:18 +0700

    handle return permission list for user login

--- src/services/user.services.js
+++ src/services/user.services.js
@@ -19,6 +19,38 @@ export default {
     }
   },
 
+  getUserWithPerissionByEmail: async (email) => {
+    try {
+      const userResults = await prisma.user.findUnique({ where: { email } });
+      // get all permits of user
+      const permits = await prisma.permit.findMany({
+        where: {
+          userId: userResults.id,
+        },
+        select: {
+          permission: {
+            select: {
+              id: true,
+              permissionName: true,
+            },
+          },
+        },
+      });
+
+      // get permission list Id
+      const permitList = permits.reduce((acc, curr) => {
+        acc.push(curr.permission['id']);
+        return acc;
+      }, []);
+
+      // assign permitList for user result
+      userResults.permitList = permitList;
+      return Promise.resolve(userResults);
+    } catch (err) {
+      throw err;
+    }
+  },
+
   updateUserById: async (userId, dataUpdateInvo) => {
     try {
       const dataUpdate = {};
