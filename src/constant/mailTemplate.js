cb07b70916cfc029b0909ea2d8a157ef5e7b750c
PhucHoangVan <20521760@gm.uit.edu.vn>
2023-11-18 23:30:54 +0700

    add template for send mail to reset password

--- src/constant/mailTemplate.js
+++ src/constant/mailTemplate.js
@@ -3,6 +3,7 @@ dotenv.config();
 
 // subject template mail create staff account
 export const MAIL_CREATE_SUBJECT = '[MediServe provide staff Account]';
+export const MAIL_RESET_PASSWORD_SUBJECT = '[MediServe reset password]';
 
 export const templateCreateAccount = (name = '', password) => {
   return `
@@ -94,10 +95,84 @@ export const templateCreateAccount = (name = '', password) => {
           <p>Use the above password to log in to the MediServe management application!</p>
           <p><b>Thank you, <br/>Admin MediServe system!</b></p>
           <div class="wrap-button">
-              <a href="${process.env.APP_LOGIN_URL}" class="login-button">Login MediServe</a>
+              <a href="${process.env.CLIENT_URL}/login" class="login-button">Login MediServe</a>
           </div>
       </div>
   </body>
   </html>
     `;
+};
+
+export const templateResetPassword = (name, token) => {
+  return `
+  <!DOCTYPE html>
+  <html lang="en">
+  <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Email Template</title>
+      <style>
+          body {
+              display: flex;
+              margin: 0;
+              padding: 0;
+              color: #000000;
+              font-family: Arial, sans-serif;
+          }
+  
+          .container {
+              max-width: 600px;
+              margin: auto auto;
+              padding: 30px;
+              border: #f0f0f0 2px solid;
+              border-radius: 8px;
+              background-color: #f6fafc;
+          }
+  
+          p {
+              line-height: 1.4;
+              margin-bottom: 15px;
+          }
+  
+          h1 {
+              padding: 0;
+              margin: 0;
+              font-size: 26px;
+          }
+  
+          hr {
+              border-color: #ffffff;
+          }
+  
+          .wrap-button {
+              display: flex;
+          }
+  
+          .login-button {
+              display: inline-block;
+              background-color: #3AAEE0;
+              padding: 10px 20px;
+              border-radius: 4px;
+              font-weight: bold;
+              text-align: center;
+              color: #FFFFFF;
+              text-decoration: none;
+              margin: 15px auto 0;
+              box-shadow: 0 5px 10px rgba(58, 174, 224, 0.5);
+          }
+  
+      </style>
+  </head>
+  <body>
+      <div class="container">
+          <h1>Hello ${name},</h1>
+          <p>You have requested to reset your password for the <b style="color: rgb(244, 131, 61);">MediServe</b> system!</p>
+          <hr/>
+          <p>You cannot simply send you your old password. A unique link to reset your password has been generated for you. To reset your password, click the following link!</p>
+          <div class="wrap-button">
+              <a href="${process.env.CLIENT_URL}/reset-password/${token}" class="login-button">Reset Password</a>
+          </div>
+      </div>
+  </body>
+  </html>`;
 };
