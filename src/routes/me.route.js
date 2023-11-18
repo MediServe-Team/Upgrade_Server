dd53eaf54334ef8019ed4455666181bbdcc575ef
PhucHoangVan <20521760@gm.uit.edu.vn>
2023-11-18 23:33:36 +0700

    handle api for fogot password and reset password

--- src/routes/me.route.js
+++ src/routes/me.route.js
@@ -1,12 +1,18 @@
 import express from 'express';
 const router = express.Router();
 import meController from '../controllers/me.controller.js';
-import { verifyAccessToken } from '../helpers/jwt.service.js';
+import { verifyAccessToken, verifyResetPasswordToken } from '../helpers/jwt.service.js';
 
 //* [GET] /me/profile   -> Get my profile
 router.get('/profile', verifyAccessToken, meController.getProfile);
 
 //* [PUT] /me/update-info     -> Edit my profile
 router.put('/update-info', verifyAccessToken, meController.updateInfo);
+
+//* [PUT] /me/change-password   -> Request change password
+router.put('/forgot-password', meController.forGotPassword);
+
+//* [PUT] /me/reset-password/:token -> reset password
+router.put('/reset-password/:token', verifyResetPasswordToken, meController.resetPassword);
 
 export default router;
--- src/services/me.services.js
+++ src/services/me.services.js
@@ -1,5 +1,12 @@
 import prisma from '../config/prisma.instance.js';
 import { removeImg, storeImg } from '../helpers/cloudinary.js';
+import createError from 'http-errors';
+import bcrypt from 'bcrypt';
+import { createResetPasswordToken } from '../helpers/jwt.service.js';
+import { sendMail } from '../helpers/nodeMailer.js';
+import { templateResetPassword, MAIL_RESET_PASSWORD_SUBJECT } from '../constant/mailTemplate.js';
+import { passwordValidate } from '../helpers/validation.js';
+import connectToRedis from '../config/redis.client.js';
 
 export default {
   getUserById: async (id) => {
@@ -88,6 +95,60 @@ export default {
         },
       });
       return Promise.resolve(returnData);
+    } catch (err) {
+      throw err;
+    }
+  },
+
+  forGotPassword: async (email) => {
+    try {
+      //* check email aready exists
+      const userInfo = await prisma.user.findUnique({ where: { email } });
+      if (!userInfo) {
+        throw createError.NotFound('Email does not exist.');
+      }
+
+      //* create reset password token
+      const resetPassToken = await createResetPasswordToken(email);
+
+      //* send to mail reset password url
+      if (resetPassToken) {
+        try {
+          await sendMail(email, MAIL_RESET_PASSWORD_SUBJECT, templateResetPassword(userInfo.fullName, resetPassToken));
+        } catch (err) {
+          throw err;
+        }
+      }
+
+      Promise.resolve(true);
+    } catch (err) {
+      throw err;
+    }
+  },
+
+  resetUserPasswordByEmail: async (email, password) => {
+    try {
+      //* validate password
+      const { error } = passwordValidate({ password });
+      if (error) {
+        throw createError(error.details[0].message);
+      }
+
+      //* hash password
+      const salt = await bcrypt.genSalt(10);
+      const hashPassword = await bcrypt.hash(password, salt);
+
+      //* update password
+      const userUpdated = await prisma.user.update({
+        where: { email },
+        data: { password: hashPassword },
+      });
+
+      //* delete token stored before in redis
+      if (userUpdated) {
+        const redis = await connectToRedis();
+        await redis.del(email);
+      }
     } catch (err) {
       throw err;
     }
